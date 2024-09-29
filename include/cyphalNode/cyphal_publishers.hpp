2864801c932ebf7fba2bc05f7bb80492346156f7
PonomarevDA <ponomarevda96@gmail.com>
2024-09-29 23:54:23 +0300

    improve compact feedback and publisher

--- include/cyphalNode/cyphal_publishers.hpp
+++ include/cyphalNode/cyphal_publishers.hpp
@@ -18,19 +18,9 @@ class Cyphal;
 
 class CyphalPublisher {
 public:
-    CyphalPublisher(Cyphal* driver_, CanardPortID port_id_) : driver(driver_) {
+    CyphalPublisher();
+    CyphalPublisher(Cyphal* driver_, CanardPortID port_id_);
 
-        transfer_metadata.priority = CanardPriorityNominal;
-        transfer_metadata.transfer_kind = CanardTransferKindMessage;
-        transfer_metadata.port_id = port_id_;
-        transfer_metadata.remote_node_id = CANARD_NODE_ID_UNSET;
-        transfer_metadata.transfer_id = 0;
-
-        if (publishers_amount < MAX_PUB_NUM) {
-            publishers[publishers_amount] = this;
-            publishers_amount++;
-        }
-    }
     void setPortId(CanardPortID port_id);
     CanardPortID getPortId() const;
     bool isEnabled() const;
--- include/cyphalNode/zubax/compact_feedback.hpp
+++ include/cyphalNode/zubax/compact_feedback.hpp
@@ -10,9 +10,17 @@
 
 namespace zubax::telega {
 
+struct CompactFeedback {
+    float dc_voltage;
+    float dc_current;
+    float phase_current_amplitude;
+    int32_t velocity;
+    int8_t demand_factor_pct;
+};
+
 // https://telega.zubax.com/interfaces/cyphal.html#compact
 #pragma pack(push, 1)
-struct CompactFeedback {
+struct RawCompactFeedback {
     uint32_t dc_voltage             : 11;
     int32_t dc_current              : 12;
     int32_t phase_current_amplitude : 12;
@@ -22,11 +30,26 @@ struct CompactFeedback {
 #pragma pack(pop)
 
 struct CompactFeedbackPublisher: public cyphal::CyphalPublisher {
-    CompactFeedbackPublisher() : cyphal::CyphalPublisher(cyphal::Cyphal::get_instance(), 65535) { }
-    void publish(const CompactFeedback& msg);
-    static constexpr float RPM_TO_RAD_PER_SEC = 0.1047198;
-private:
-    static constexpr size_t zubax_telega_CompactFeedback_0_1_SERIALIZATION_BUFFER_SIZE_BYTES = 7;
+    CompactFeedbackPublisher() = default;
+
+    inline void publish() {
+        constexpr size_t zubax_telega_CompactFeedback_0_1_SERIALIZATION_BUFFER_SIZE_BYTES = 7;
+        constexpr float RPM_TO_RAD_PER_SEC = 0.1047198;
+        constexpr float VOLTAGE_SERIALIZE = 5.0f;
+        constexpr float CURRENT_SERIALIZE = 5.0f;
+
+        RawCompactFeedback buffer = {
+            .dc_voltage = static_cast<uint32_t>(msg.dc_voltage * VOLTAGE_SERIALIZE),
+            .dc_current = static_cast<int32_t>(msg.dc_current * CURRENT_SERIALIZE),
+            .phase_current_amplitude = (int32_t)(msg.phase_current_amplitude * CURRENT_SERIALIZE),
+            .velocity = static_cast<int32_t>(msg.velocity * RPM_TO_RAD_PER_SEC),
+            .demand_factor_pct = msg.demand_factor_pct,
+        };
+
+        push(zubax_telega_CompactFeedback_0_1_SERIALIZATION_BUFFER_SIZE_BYTES, (const uint8_t*)&buffer);
+    }
+
+    CompactFeedback msg;
 };
 
 }  // namespace zubax::telega
